5f4ee8429c88458d51ebe72adc6dba567964f0de
elvis-chaves <155856275+elvis-chaves@users.noreply.github.com>
2026-02-07 11:20:46 -0300

    Add files via upload

---
+++ Aula 7/exercicio_7.py
@@ -0,0 +1,27 @@
+
+lista = list(range(2,21,2))
+print(lista)
+
+numeros = [1,2,3,4,5,6,7,8,9,10]
+print(numeros)
+
+print(numeros[2])
+
+numeros.append(9)
+print(numeros)
+
+numeros.remove(5)
+print(numeros)
+
+#numeros.pop(4)
+#print(numeros)
+
+carros = ['HB20','HRV','X1']
+
+print(carros , numeros)
+
+carros.extend(numeros)
+print(carros)
+
+
+
